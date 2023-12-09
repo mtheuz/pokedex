30805ad4231c2dd70993b79ffa046089f5f7cb49
Matheus Mota <matheuzwork@gmail.com>
2023-12-09 18:00:04 -0300

    feat: function for get all pokemons

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import Search from './components/Search/Search'
 import NotFound from './components/NotFound/NotFound'
+import { getAllPokemons } from './api/api'
 
 function App() {
   const[searchResult, setSearchResult] = useState([])
+  const[allPokemons, setAllPokemons] = useState(" ")
+
+  const fetchPokemons = async () =>{
+    const resultAllPokemons = await getAllPokemons()
+    console.log(resultAllPokemons)
+    setAllPokemons(resultAllPokemons)
+  }
+
+  useEffect(()=>{
+    fetchPokemons()
+  },[])
+
   return (
     <>
       <NavBar/>
--- pokedex/src/api/api.jsx
+++ pokedex/src/api/api.jsx
@@ -11,3 +11,16 @@ export  const searchPokemon = async (pokemon) => {
         }
     }
 }
+export  const getAllPokemons = async (limit= 50, offset=0) => {
+    try {
+        let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}.`
+        const response = await fetch(url)
+        return await response.json()
+
+    } catch (error) {
+        console.log('erro message:', error)
+        if(error.message.includes('Not Found')){
+            return false
+        }
+    }
+}
